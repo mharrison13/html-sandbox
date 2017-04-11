413c5f7c880955ee9ac76690e7355fec0885c6a9
Michael Harrison <mharrison13@cnm.edu>
2017-04-11 16:12:56 -0600

    first save

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
-
+		<meta charset="utf-8" />
+		<title>HTML Sandbox</title>
 	</head>
 	<body>
-
+		<h1>An HTML Sandbox</h1>
 	</body>
 </html>
