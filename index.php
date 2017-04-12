2679dfd2d65b8325cc9c2a7283e8ba169ff4d512
Michael Harrison <mharrison13@cnm.edu>
2017-04-12 09:27:03 -0600

    added lists

--- index.php
+++ index.php
@@ -19,11 +19,26 @@
 
 		<main>
 			<h2>I missed Taco Tuesday!</h2>
-				<p>Every day is taco ipsum tuesday. Say taco one more time. If you were a taco, would you eat yourself? Side of rice and beans, please. very day is taco ipsum tuesday. Say taco one more time. If you were a taco, would you eat yourself? Side of rice and beans, please. Can you put some peppers and onions on that? Tacos, again? This will be 5 times this week and it’s onCan you put some peppers and onions on that? Tacos, again? This will be 5 times very day is taco ipsum tuesday. Say taco one more time. If you were a taco, would you eat yourself? Side of rice and beans, please. Can you put some peppers and onions on that? Tacos, again? This will be 5 times this week and it’s onthis week and it’s only Tuesday. Make it a double there pal. Make it a double there pal. If you were a taco, </p>
-				<p>Give me all your tacos. Let’s do a beef and a chicken, and one with both. Carne asada on corn tortillas. Yeah, apparently the taco shack was robbed. They left the money but took the tacos. CARNITAS!! Tacos Al pastor/De Adobada are made of thin pork very day is taco ipsum tuesday. Say taco one more time. If you were a taco, would you eat yourself? Side of rice and beans, please. Can you put some peppers and onions on that? Tacos, again? This will be 5 times this week and it’s onvery day is taco ipsum tuesday. Say taco one more time. If you were a taco, would you eat yourself? Side of rice and beans, please. Can you put some peppers and onions on that? Tacos, again? This will be 5 times this week and it’s onsteaks seasoned with adobo seasoning.</p>
+			<p>Every day is taco ipsum tuesday. Say taco one more time. If you were a taco, would you eat yourself? Side of rice and beans, please. very day is taco ipsum tuesday. Say taco one more time. If you were a taco, would you eat yourself? Side of rice and beans, please. Can you put some peppers and onions on that? Tacos, again? This will be 5 times this week and it’s onCan you put some peppers and onions on that? Tacos, again? This will be 5 times very day is taco ipsum tuesday. Say taco one more time. If you were a taco, would you eat yourself? Side of rice and beans, please. Can you put some peppers and onions on that? Tacos, again? This will be 5 times this week and it’s onthis week and it’s only Tuesday. Make it a double there pal. Make it a double there pal. If you were a taco, </p>
+			<p>Give me all your tacos. Let’s do a beef and a chicken, and one with both. Carne asada on corn tortillas. Yeah, apparently the taco shack was robbed. They left the money but took the tacos. CARNITAS!! Tacos Al pastor/De Adobada are made of thin pork very day is taco ipsum tuesday. Say taco one more time. If you were a taco, would you eat yourself? Side of rice and beans, please. Can you put some peppers and onions on that? Tacos, again? This will be 5 times this week and it’s onvery day is taco ipsum tuesday. Say taco one more time. If you were a taco, would you eat yourself? Side of rice and beans, please. Can you put some peppers and onions on that? Tacos, again? This will be 5 times this week and it’s onsteaks seasoned with adobo seasoning.</p>
 			<img src="images/taco.jpg" alt="tacos are great" width="500"/>
 			<p>This is a link to Tacos: <a href="http://www.tacos.com" target="_blank">Tacos</a> </p>
 			<p>This is a link about me: <a href="about/index.php"</p>
+
+			<h2>A few of my favorite things:</h2>
+			<ul>
+				<li>Cars</li>
+				<li>Computers</li>
+				<li>Movies</li>
+				<li>Camping</li>
+			</ul>
+
+			<h2>A few of my less favorite things:</h2>
+			<ol>
+				<li>homework</li>
+				<li>yard work</li>
+				<li>regular work</li>
+			</ol>
 
 		</main>
 
