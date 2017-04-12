a04b7e2c6b7512de8e59851d6f1651411d25e2e4
Michael Harrison <mharrison13@cnm.edu>
2017-04-12 08:21:42 -0600

    first save

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
+
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+
+
 		<title>HTML Sandbox</title>
 	</head>
 	<body>
