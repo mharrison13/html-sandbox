ac1a1cacc2f78c3771e16c4a4310a0392f29845c
Michael Harrison <mharrison13@cnm.edu>
2017-04-12 08:56:11 -0600

    added images

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 			<h2>I missed Taco Tuesday!</h2>
 				<p>Every day is taco ipsum tuesday. Say taco one more time. If you were a taco, would you eat yourself? Side of rice and beans, please. Can you put some peppers and onions on that? Tacos, again? This will be 5 times this week and it’s only Tuesday. Make it a double there pal. Make it a double there pal. If you were a taco, </p>
 				<p>Give me all your tacos. Let’s do a beef and a chicken, and one with both. Carne asada on corn tortillas. Yeah, apparently the taco shack was robbed. They left the money but took the tacos. CARNITAS!! Tacos Al pastor/De Adobada are made of thin pork steaks seasoned with adobo seasoning.</p>
-			<img src="images/taco.jpg" alt="tacos are great" width="500"/>
+			<img src="images/taco.jpg" alt="tacos are great"/>
 
 		</main>
 
