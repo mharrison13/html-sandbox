ace7c5b02094f7b4709427bea35e653857687566
Michael Harrison <mharrison13@cnm.edu>
2017-04-12 08:48:30 -0600

    added images

--- index.php
+++ index.php
@@ -9,10 +9,20 @@
 		<!-- custom JavaScript-->
 		<script type="text/javascript" src="js/scripts.js"></script>
 
-
 		<title>HTML Sandbox</title>
 	</head>
 	<body>
-		<h1>An HTML Sandbox</h1>
+
+		<header>
+			<h1>An HTML Sandbox</h1>
+		</header>
+
+		<main>
+			<h2>I missed Taco Tuesday!</h2>
+				<p>Every day is taco ipsum tuesday. Say taco one more time. If you were a taco, would you eat yourself? Side of rice and beans, please. Can you put some peppers and onions on that? Tacos, again? This will be 5 times this week and it’s only Tuesday. Make it a double there pal. Make it a double there pal. If you were a taco, </p>
+				<p>Give me all your tacos. Let’s do a beef and a chicken, and one with both. Carne asada on corn tortillas. Yeah, apparently the taco shack was robbed. They left the money but took the tacos. CARNITAS!! Tacos Al pastor/De Adobada are made of thin pork steaks seasoned with adobo seasoning.</p>
+
+		</main>
+
 	</body>
 </html>
